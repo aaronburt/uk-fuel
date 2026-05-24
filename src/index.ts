7a8f1bf705f6b4337672d2a565e61b1a5d75edb3
aaronburt <email@aaronburt.co.uk>
2026-05-24 19:11:12 +0100

    fix: cache updated to one hour to reduce calls

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.get("/", async (c) => {
     return new Response(cachedData, {
       headers: {
         "Content-Type": "application/json",
-        "Cache-Control": "public, max-age=900"
+        "Cache-Control": "public, max-age=3600"
       }
     });
   } catch (error: unknown) {
